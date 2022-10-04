d8e73900f22cb46a6875f0353cc9d70036245bd8
김동우 <sunbi5252@naver.com>
2022-10-05 01:53:03 +0900

    feat: add spark basic

--- spark_test/3_spark_load_mysql.py
+++ spark_test/3_spark_load_mysql.py
@@ -15,11 +15,10 @@ spark = SparkSession.builder.getOrCreate()
 # df.select("uid", "event_type").write.format("jdbc").option("url", "jdbc:mysql://j7e202.p.ssafy.io:3306/mlbti").ption("driver", "com.mysql.jdbc.cj.Driver").option("dbtable", "sample_table").option("user", "ssafy").option("password", "!321yfass").save()
 sql_url = "j7e202.p.ssafy.io"
 database = "mlbti"
-table = "pitchers"
+table = "users"
 user = "ssafy"
 password = "!321yfass"
 jdbc = spark.read.format("jdbc").option("driver", "com.mysql.cj.jdbc.Driver").option("url", "jdbc:mysql://{}:3306/{}?serverTimezone=Asia/Seoul ".format(sql_url, database)).option("user", user).option("password", password).option("dbtable", table).load()
-
 print(type(jdbc))
 jdbc.show(10)
 # df.toDF('uid', 'player_type', 'player_uid', 'date', 'game_uid', 'season', 'weather', 'opponent_uid', 'event_index', 'event_type', 'player_main_position', 'opponent_hand', 'rbi', 'strikes', 'balls', 'outs', 'inning', 'is_top_inning', 'type', 'creater', 'updater','create_time','update_time') # column name 변경
