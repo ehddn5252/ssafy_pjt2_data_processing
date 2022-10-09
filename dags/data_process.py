c41269763606718206a4c8152853da8b738cb0c9
김동우 <sunbi5252@naver.com>
2022-10-09 21:51:44 +0900

    feat: remove error dag

--- dags/data_process.py
+++ dags/data_process.py
@@ -534,7 +534,7 @@ with DAG(**dag_args) as dag:
         bash_command='echo "complete~!"',
         trigger_rule=TriggerRule.NONE_FAILED
     )
-    _stack_schedules >> _stack_raw_data >> _stack_event_table_from_raw_data >> _stack_event_players_from_events >> _update_pitcher_position >> _delete_pitchers_season_data >> _stack_pitchers_from_event_pitchers >> _update_pitcher_position >> complete
-    #_stack_schedules >> _stack_raw_data >> _stack_event_table_from_raw_data >> _stack_event_players_from_events >> _update_pitcher_position >> _delete_pitchers_season_data
+    # _stack_schedules >> _stack_raw_data >> _stack_event_table_from_raw_data >> _stack_event_players_from_events >> _update_pitcher_position >> _delete_pitchers_season_data >> _stack_pitchers_from_event_pitchers >> _update_pitcher_position >> complete
+    _stack_schedules >> _stack_raw_data >> _stack_event_table_from_raw_data >> _stack_event_players_from_events >> _update_pitcher_position >> _delete_pitchers_season_data >> _update_pitcher_position >> complete
 
     # start >> now_date >> stack_schedules >> complete
